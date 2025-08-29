cc858ed3b164b11193e428e7ba88bde74a34cdc7
Evan Meeks <evanmeeks@users.noreply.github.com>
2025-08-29 09:45:00 -0600

    feat: add password reset functionality

---
+++ app/password-reset.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { PasswordResetScreen } from '@/components/screens/PasswordResetScreen';
+
+export default function PasswordResetPage() {
+  return <PasswordResetScreen />;
+}
